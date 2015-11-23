be14d3c402d45f6ca3ba37930c2103ef4db56f9e
Adam Chivers <bigsnake09@googlemail.com>
2015-11-23 01:55:16 +0000

    Converted rotateObjects to C#

---
+++ Assets/Source/Utils/ObjectRotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectRotator : MonoBehaviour {
+
+    public Vector3 rotSpeed;
+
+    void LateUpdate()
+    {
+        transform.Rotate(rotSpeed * Time.deltaTime);
+    }
+}
